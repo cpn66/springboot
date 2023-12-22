f70707231c6198a419e8d00f571ecdcf1fa66c96
cpn@cpn-work <cpn@ugtel.ru>
2023-12-22 14:36:52 +0300

    POST GET работает

--- src/main/kotlin/com/example/demo/HelloController.kt
+++ src/main/kotlin/com/example/demo/HelloController.kt
@@ -1,11 +1,24 @@
 package com.example.demo
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-
+data class ViewAccount(
+  val id: Int,
+  val name: String
+)
+data class CreateAccount(
+    val name: String
+)
 @RestController
-class HelloController {
-    @GetMapping("/hello")
-    fun hello(): String=
-        "Hello World"
+@RequestMapping("/accounts")
+class AccountController {
+    @GetMapping("/")
+    fun getAll(): Iterable<ViewAccount> =
+        listOf(ViewAccount(id=1,name="first"))
+    @PostMapping("/")
+    fun create(@RequestBody request: CreateAccount): ViewAccount =
+        ViewAccount(id=2, name=request.name)
 }
