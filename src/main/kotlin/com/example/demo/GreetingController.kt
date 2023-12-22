a1b330f843666914d27997361d6f92b8fc640e04
cpn@cpn-work <cpn@ugtel.ru>
2023-12-22 17:03:40 +0300

    @Value application-dev.properties

--- src/main/kotlin/com/example/demo/GreetingController.kt
+++ src/main/kotlin/com/example/demo/GreetingController.kt
@@ -1,12 +1,17 @@
 package com.example.demo
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 data class Message(
-    val message: String
+    val message: String,
+    val buildNumber: String,
 )
 @RestController
-class GreetingController {
+class GreetingController (
+    @Value("\${vladsave.firstboot.buildnumber}") val buildNumber: String
+        )
+{
     @GetMapping("/hello")
-    fun hello():Message=Message("Hello World")
+    fun hello():Message=Message("Hello World",buildNumber)
 }
